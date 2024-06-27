60091f6db709ad0525603263e24d898e3dab7f25
Aurrum <aurrum64@gmail.com>
2024-06-27 15:33:57 -0400

    Introduced TextExpander / Converted SideNavigation into a client component to support active links

---
+++ app/_components/text-expander.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { PropsWithChildren, useState } from "react";
+
+function TextExpander({ children }: PropsWithChildren) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const displayText = isExpanded
+    ? children
+    : (children as string).split(" ").slice(0, 40).join(" ") + "...";
+
+  return (
+    <span>
+      {displayText}{" "}
+      <button
+        className="border-b border-primary-700 pb-1 leading-3 text-primary-700"
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        {isExpanded ? "Show less" : "Show more"}
+      </button>
+    </span>
+  );
+}
+
+export default TextExpander;
